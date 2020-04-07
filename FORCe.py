66e07040333b78f482e98e4e3d8fef4dddcf9f9d
Naomi Bassett <naomi.bassett12@imperial.ac.uk>
2020-04-07 18:10:38 +0100

    start extracting features from projected ICs for thresholding: amplitude thresholds, kurtosis, power spectrum info, standard deviation features, AMI

--- FORCe.py
+++ FORCe.py
@@ -8,9 +8,12 @@ Returns:
 """
 
 import numpy as np
+from numpy.fft import fft
+from scipy.stats import kurtosis
 
 from electrode_locations import electrode_locations
 from decomposition import applyWaveletICA
+from automutual_information import extractFeaturesMultiChsWaveAMI
 
 import pdb 
 
@@ -45,7 +48,15 @@ def estimate_removed_channels(EEGdata, remChs, electrodes):
         
     return EEGdata
 
-
+def powerspectrum(data, Fs):
+    """ compute power spectrum of data """
+    L = data.shape[0]
+    NFFT = 2**np.ceil(np.log2(L)) 
+    Y = fft(data, int(NFFT)) / L
+    f = Fs/2*np.linspace(0,1,NFFT/2)
+    power = 2 * abs(Y[:int(NFFT/2)])
+    p = np.array([f,power])
+    return p
 
 def FORCe(EEGdata, fs, electrodes):
     """ Run FORCe on EEGdata """
@@ -60,9 +71,71 @@ def FORCe(EEGdata, fs, electrodes):
     
     EEGdata = estimate_removed_channels(EEGdata, remCh, electrodes)
     
-    ICs, mixMat, wavePacks = applyWaveletICA(EEGdata)
+    ICs, mixMat, wavePacks, tNodes = applyWaveletICA(EEGdata)
     
     # TODO identify contaminated ICs for approximation coefficients
+    tN = 0 # approximation coefficients only
+    
+    remICsPT = [] # projection threshold
+    remICsP2P = [] # peak-to-peak
+    
+    projIC = []
+    entropy = []
+    specDist = []
+    gammaPSD = []
+    stdProj = []
+    stdRatio = []
+    featsClust = []
+    for iNo in range(ICs[tN].shape[0]):
+        
+        # estimate scalp projections
+        ICtemp = ICs[tN].copy()
+        for iT in range(ICtemp.shape[0]):
+            if iT != iNo:
+                ICtemp[iT,:] = np.zeros((1,ICtemp.shape[1]))
+        projIC.append( np.matmul( mixMat, ICtemp ) )
+
+        # projection thresholds
+        pthresh = []
+        p2p = []
+        for pNo in range(projIC[iNo].shape[0]):
+            signal = projIC[iNo][pNo,:]
+            pthresh.append( min(signal) < -100 or max(signal) > 100 )
+            p2p.append( max(signal) - min(signal) > 60 )
+
+        if any(pthresh): remICsPT.append(iNo)
+        if any(p2p): remICsP2P.append(iNo)
+
+        # get kurtosis value
+        entropy.append(np.mean( kurtosis( np.transpose(projIC[iNo]), fisher=False ) ))
+        
+        # power spectrum
+        specDistT = []
+        gammaPSDT = []
+        for pNo in range(projIC[iNo].shape[0]):
+            psT = powerspectrum( projIC[iNo][pNo,:], fs )
+            idealDistro = np.vstack((psT[0,:], 1./psT[0,:]))
+            idealDistro[1,0] = 0 # remove inf val for 1/0
+            # normalise
+            idealDistro[1,1:] = idealDistro[1,1:] / max(idealDistro[1,1:])
+            psCheck = psT[1,1:] / max(psT[1,1:])
+            
+            diff = psCheck - idealDistro[1,1:]
+            specDistT.append( np.sqrt( np.matmul(diff, np.transpose(diff)) ) )
+            gammaPSDT.append( np.mean( psT[1, np.where(psT[0,:] > 30)] ) )
+            
+        specDist.append(np.mean(specDistT))
+        gammaPSD.append(max(gammaPSDT))
+
+        # std and std ratio
+        stdProjT = [np.std(signal,ddof=1) for signal in projIC[iNo]]
+        stdProj.append(max(stdProjT))
+        frontChans = [i for i,c in enumerate(electrodes) if 'F' in c]
+        otherChans = [i for i,c, in enumerate(electrodes) if not 'F' in c]
+        stdRatio.append(np.mean([stdProjT[i] for i in frontChans]) / np.mean([stdProjT[i] for i in otherChans]))
+        
+        # get AMI
+        featsClust.append( extractFeaturesMultiChsWaveAMI(projIC[iNo],fs) )
     
     # TODO soft thresholding on approximation and detail
     
---
+++ automutual_information.py
@@ -0,0 +1,66 @@
+""" Extract features for Auto-Mutual Information """
+import numpy as np
+
+def minf(pab, papb):
+    eps = 1e-12
+    
+    wpab = np.where(pab > eps)
+    wpapb = np.where(papb > eps)
+    
+    n_wpab = wpab[0].shape[0]
+    n_wpapb = wpapb[0].shape[0]
+
+    rows = []
+    cols = []
+    for i in range(n_wpab):
+        for j in range(n_wpapb):
+            if (wpab[0][i] == wpapb[0][j]) and (wpab[1][i] == wpapb[1][j]): 
+                rows.append(wpab[0][i])
+                cols.append(wpab[1][i])
+    I = (np.array(rows), np.array(cols))
+    y = pab[I] * np.log2(pab[I] / papb[I]) # same values, different order from Matlab
+    return y
+
+def hist2a(A,B,L):
+    ma = min(A)
+    MA = max(A)
+    mb = min(B)
+    MB = max(B)
+    eps = 2.2204e-16
+    A = np.round((A-ma)*(L-1)/(MA-ma+eps))
+    B = np.round((B-mb)*(L-1)/(MB-mb+eps))
+    n = np.zeros((L,L))
+    for i in range(L):
+        inds = np.where(A==i)
+        a = np.digitize(B[inds], [*range(L)])
+        for k in range(len(a)):
+            n[i,a[k]-1] += 1
+    return n        
+
+def mi(A, B):
+    L = 32
+    na,_ = np.histogram(A[:],L)
+    na = na/sum(na)
+    nb,_ = np.histogram(B[:],L)
+    nb = nb/sum(nb)
+    n2 = hist2a(A,B,L)
+    n2 = n2/np.sum(n2)
+
+    t = minf( n2, np.outer(na,nb) )
+    return sum(t)
+
+def extractFeaturesMultiChsWaveAMI(signalSet, Fs):
+    """ calculate auto-mutual information (AMI) """
+    lagOffsets = [60]
+    features = np.zeros((1,len(lagOffsets)))
+    for lagNo in range(len(lagOffsets)):
+        endP = signalSet.shape[1]
+        lags = range(0, int(np.floor(endP/2)), lagOffsets[lagNo])
+        AMIs = np.zeros((signalSet.shape[0],len(lags)))
+        for chNo in range(signalSet.shape[0]):
+            for j in range(len(lags)):
+                AMIs[chNo,j] = mi( signalSet[chNo, :endP-lags[j]-1], signalSet[chNo, 1+lags[j]:endP] )
+                print('chNo: {}, j: {}'.format(chNo,j))
+        features[lagNo] = max(np.mean(AMIs,1))
+    
+    return features
